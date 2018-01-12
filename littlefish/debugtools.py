1efe2773054a541c56517bd2f41ec6499195db2d
Stephen Brown <steve@fig14.com>
2018-01-12 10:52:22 +0000

    Added debugtools

---
+++ littlefish/debugtools.py
@@ -0,0 +1,60 @@
+"""
+Debugging stuff
+"""
+
+import logging
+import inspect
+
+__author__ = 'Stephen Brown (Little Fish Solutions LTD)'
+
+log = logging.getLogger(__name__)
+
+
+class DontPrint(object):
+    pass
+
+DONT_PRINT = DontPrint()
+
+
+DEBUG_FORMAT = '@@@ {}'
+LESSER_DEBUG_FORMAT = '  @ {}'
+
+
+def lesser_debug_print(s):
+    print(LESSER_DEBUG_FORMAT.format(s))
+
+
+def debug_print(s=DONT_PRINT, **kwargs):
+    if s is not DONT_PRINT:
+        print(DEBUG_FORMAT.format(s))
+    
+    first = True
+    for k, v in kwargs.items():
+        if s is DONT_PRINT and first:
+            debug_print('{} = {}'.format(k, v))
+        else:
+            lesser_debug_print('{} = {}'.format(k, v))
+
+        first = False
+
+
+def print_spacer(level=3):
+    if level == 1:
+        c = '#'
+    elif level == 2:
+        c = '='
+    else:
+        c = '-'
+
+    print(c * 80)
+
+
+def print_location(**kwargs):
+    """
+    :param kwargs: Pass in the arguments to the function and they will be printed too!
+    """
+    stack = inspect.stack()[1]
+    debug_print('{}:{} {}()'.format(stack[1], stack[2], stack[3]))
+
+    for k, v in kwargs.items():
+        lesser_debug_print('{} = {}'.format(k, v))
