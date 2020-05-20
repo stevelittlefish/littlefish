b677e4ec1052bd3efca4f206503925dd56daf319
Stephen Brown <steve@fig14.com>
2020-05-20 02:38:27 +0100

    made lxml optional too

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     name='littlefish',
     packages=['littlefish', 'littlefish.background', 'littlefish.sessiondata'],
     include_package_data=True,
-    version='0.0.50',
+    version='0.0.52',
     description='Flask webapp utility functions by Little Fish Solutions LTD',
     author='Stephen Brown (Little Fish Solutions LTD)',
     author_email='opensource@littlefish.solutions',
     url='https://github.com/stevelittlefish/littlefish',
-    download_url='https://github.com/stevelittlefish/littlefish/archive/v0.0.50.tar.gz',
+    download_url='https://github.com/stevelittlefish/littlefish/archive/v0.0.52.tar.gz',
     keywords=['flask', 'utility', 'time', 'pager'],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
@@ -34,12 +34,22 @@ setup(
         'Flask>=0.12.0',
         'Flask-SQLAlchemy>=2.0',
         'Jinja2>=2.9.0',
-        'lxml>=3.7.0',
         'IPy>=0.83',
-        'redis>=3.2.1',
-        'celery>=4.3.0',
-        'endesive>=1.1.5',
-        'pyOpenSSL>=19.0.0'
-    ]
+    ],
+    extras_require={
+        'pdfsigner': [
+            'endesive>=1.1.5',
+            'pyOpenSSL>=19.0.0'
+        ],
+        'celery': [
+            'celery>=4.3.0'
+        ],
+        'redis': [
+            'redis>=3.2.1'
+        ],
+        'xml': [
+            'lxml>=3.7.0'
+        ]
+    }
 )
 
