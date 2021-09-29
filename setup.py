1692909c850f1dad0676f306ed82518b73524cb5
Stephen Brown <steve@fig14.com>
2021-09-29 11:53:54 +0100

    version bump

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     name='littlefish',
     packages=['littlefish', 'littlefish.background', 'littlefish.sessiondata'],
     include_package_data=True,
-    version='0.0.61',
+    version='0.0.62',
     description='Flask webapp utility functions by Little Fish Solutions LTD',
     author='Stephen Brown (Little Fish Solutions LTD)',
     author_email='opensource@littlefish.solutions',
     url='https://github.com/stevelittlefish/littlefish',
-    download_url='https://github.com/stevelittlefish/littlefish/archive/v0.0.61.tar.gz',
+    download_url='https://github.com/stevelittlefish/littlefish/archive/v0.0.62.tar.gz',
     keywords=['flask', 'utility', 'time', 'pager'],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
@@ -35,7 +35,8 @@ setup(
         'Flask-SQLAlchemy>=2.0',
         'Jinja2>=2.9.0',
         'IPy>=0.83',
-        'lxml>=3.7.0'
+        'lxml>=3.7.0',
+        'PyYAML>=5.3.1'
     ],
     extras_require={
         'pdfsigner': [
